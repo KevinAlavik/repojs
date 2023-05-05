7e3df6ce88c3ea0f8d6537296c554e15ec74f7b2
Kevin Alavik <kevin@alavik.se>
2023-05-05 06:38:16 +0000

    added some parsing and fetching functions

--- src/stable/index.js
+++ src/stable/index.js
@@ -7,9 +7,59 @@
 */
 
 const repojs = {
-  init: function() {
-    window.alert("Successfully started the repojs process");
+  init: function () {
+    console.log("Successfully started the repojs process");
+  },
+  repo: {
+    fetch: async function (url) {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+      } catch (error) {
+        console.error('Error fetching URL:', error);
+        return null;
+      }
+    },
+    parse: function (jsonData, pathString) {
+      let finalData;
+      if (!pathString) {
+        finalData = JSON.stringify(jsonData);
+        console.log(finalData);
+        return finalData;
+      }
+
+      const path = pathString.split(".");
+      let currentData = jsonData;
+      for (let i = 0; i < path.length; i++) {
+        const pathSegment = path[i];
+        if (pathSegment.includes("[")) {
+          const arrayPath = pathSegment.split("[");
+          const arrayName = arrayPath[0];
+          const index = parseInt(arrayPath[1].replace("]", ""));
+          currentData = currentData[arrayName][index];
+        } else {
+          currentData = currentData[pathSegment];
+        }
+      }
+
+      if (Array.isArray(currentData)) {
+        const arrayPath = path[path.length - 1].split("[");
+        const arrayName = arrayPath[0];
+        const arrayValues = currentData.map((item) => item[arrayName]);
+        finalData = JSON.stringify(arrayValues);
+      } else {
+        finalData = JSON.stringify(currentData);
+      }
+
+      console.log(finalData);
+      return finalData;
+    },
+    document: {
+      append: function (data, element) {
+        const formattedData = JSON.stringify(JSON.parse(data), null, 2);
+        element.innerHTML = '<pre>' + formattedData + '</pre>';
+      }
+    }
   }
 };
-
-window.alert("[REPO JS MESSAGE] Hello there, repojs is currently under developement! So expect bugs");
