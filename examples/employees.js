29fd95b4462db71c3b693cc5702e37815636de29
Kevin Alavik <kevin@alavik.se>
2023-06-06 19:28:33 +0100

    Added an README.md for examples

--- examples/employees.js
+++ examples/employees.js
@@ -1,8 +1,10 @@
-const repojs = require('repojs');
+const repojs = require("../src/index");
 
-const url = 'https://raw.githubusercontent.com/KevinAlavik/json-examples/main/employees.json';
+const url =
+  "https://raw.githubusercontent.com/KevinAlavik/json-examples/main/employees.json";
 
-repojs.repo.get(url)
+repojs.repo
+  .get(url)
   .then((jsonData) => {
     const employees = repojs.repo.parse(jsonData).employees;
     for (let i = 0; i < employees.length; i++) {
@@ -15,9 +17,11 @@ repojs.repo.get(url)
       console.log(`Age: ${employee.other.age}`);
       console.log(`Gender: ${employee.other.gender}`);
       console.log(`Education: ${employee.other.education}`);
-      console.log(`Languages: ${employee.other.languages.join(', ')}`);
-      console.log(`Certifications: ${employee.other.certifications.join(', ')}`);
-      console.log(`Hobbies: ${employee.other.hobbies.join(', ')}\n`);
+      console.log(`Languages: ${employee.other.languages.join(", ")}`);
+      console.log(
+        `Certifications: ${employee.other.certifications.join(", ")}`
+      );
+      console.log(`Hobbies: ${employee.other.hobbies.join(", ")}\n`);
     }
   })
   .catch((error) => {
--- examples/simple.js
+++ examples/simple.js
@@ -1,8 +1,8 @@
-const repojs = require('repojs');
+const repojs = require("../src/index");
 
 const url = "https://usescarlet.com/scarlet.json";
 
 repojs.repo.get(url).then((data) => {
-    repo = repojs.repo.parse(data);
-    console.log(`Repo Name: ${repo.META.repoName}`)
-})
+  repo = repojs.repo.parse(data);
+  console.log(`Repo Name: ${repo.META.repoName}`);
+});
